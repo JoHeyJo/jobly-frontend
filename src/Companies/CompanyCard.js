ca6ce655488ef4ed5a2257dbcd8c0b430f30db4b
jf <jpf0628@gmail.com>
2022-05-06 15:09:14 -0700

    DONE w/ hamburger  menu

--- src/Companies/CompanyCard.js
+++ src/Companies/CompanyCard.js
@@ -12,11 +12,11 @@ import "./companyCard.css";
 function CompanyCard({ company }) {
   console.log(company);
   return (
-    <Link className="link" to={`/companies/${company.handle}`}>
+    <Link id="link" to={`/companies/${company.handle}`}>
       <div className="CompanyCard">
-        <h4 className="link">{company.name}</h4>
+        <h4 id="link">{company.name}</h4>
         <img src={company.logoUrl} alt=""></img>
-        <p className="link">{company.description}</p>
+        <p id="link">{company.description}</p>
         <span style={{ padding: "5px" }} />
       </div>
     </Link>
--- src/Routes/Navigation.js
+++ src/Routes/Navigation.js
@@ -24,42 +24,101 @@ function Navigation({ logout, updateLocation }) {
         </NavLink>
       </div>
       {currentUser ? (
-        <div className="nav-right">
-          <NavLink
-            className={({ isActive }) => (isActive ? "linkActive" : "link")}
-            to="/companies"
-          >
-            Companies
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? "linkActive" : "link")}
-            to="/jobs"
-          >
-            Jobs
-          </NavLink>
+        <>
+        <input type="checkbox" id="hamburger-input" class="burger-shower" />
+          <label id="hamburger-menu" for="hamburger-input">
+            <nav id="sidebar-menu">
+              <ul style={{listStyle:"none"}}>
+                <li>
+                  <NavLink
+                    className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                    to="/"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                  <li>           
+                    <NavLink
+                    className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                    to="/companies"
+                  >
+                    Companies
+                  </NavLink>
+                  </li>
+                <li>
+                    <NavLink
+                      className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                      to="/jobs"
+                    >
+                      Jobs
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                      className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                      to="/appliedjobs"
+                    >
+                      Applied
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                      className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                      to="/profileform"
+                    >
+                      Profile
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                      className={({ isActive }) => (isActive ? "linkActive" : "link")}
+                      to="/"
+                      onClick={logout}
+                    >
+                      Log Out {currentUser.username}
+                    </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </label>
+          <div className="nav-right">
+            <NavLink
+              className={({ isActive }) => (isActive ? "linkActive" : "link")}
+              to="/companies"
+            >
+              Companies
+            </NavLink>
 
-          <NavLink
-            className={({ isActive }) => (isActive ? "linkActive" : "link")}
-            to="/appliedjobs"
-          >
-            Applied
-          </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "linkActive" : "link")}
+              to="/jobs"
+            >
+              Jobs
+            </NavLink>
 
-          <NavLink
-            className={({ isActive }) => (isActive ? "linkActive" : "link")}
-            to="/profileform"
-          >
-            Profile
-          </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? "linkActive" : "link")}
+              to="/appliedjobs"
+            >
+              Applied
+            </NavLink>
 
-          <NavLink
-            className={({ isActive }) => (isActive ? "linkActive" : "link")}
-            to="/"
-            onClick={logout}
-          >
-            Log Out {currentUser.username}
-          </NavLink>
-        </div>
+            <NavLink
+              className={({ isActive }) => (isActive ? "linkActive" : "link")}
+              to="/profileform"
+            >
+              Profile
+            </NavLink>
+
+            <NavLink
+              className={({ isActive }) => (isActive ? "linkActive" : "link")}
+              to="/"
+              onClick={logout}
+            >
+              Log Out {currentUser.username}
+            </NavLink>
+          </div>
+        </>
       ) : (
         <div className="nav-right">
           <NavLink
